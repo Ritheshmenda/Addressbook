e421a4792db8d15f7736cc6a09b8fe6137daee9c
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-28 22:55:38 +0530

    [Rithesh]added:contactcreation

---
+++ addressbook/AddressDetails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace addressbook
+{
+    class AddressDetails
+    {
+        string Name, Lastname, Address, State, Emailid;
+        int Zipcode;
+        long Mobile;
+        public AddressDetails(string name, string lastname, string address, string state, long mobile, int zipcode, string emailid)
+        {
+            this.Name = name;
+            this.Lastname = lastname;
+            this.Address = address;
+            this.State = state;
+            this.Mobile = mobile;
+            this.Zipcode = zipcode;
+            this.Emailid = emailid;
+        }
+        public void Contacts()
+        {
+            Console.WriteLine("Name : " + this.Name + "\n" + "last name : " + this.Lastname + "\n" + "address :" + this.Address + "\n" + "state : " + this.State + "\n" + "Mobile :" + this.Mobile + "\n"+"zipcode : "+this.Zipcode+"\n"+"emailid : "+this.Emailid);
+        }
+    }
+}
--- addressbook/Program.cs
+++ addressbook/Program.cs
@@ -5,8 +5,10 @@ namespace addressbook
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to the addessbook");
+        {   
+                AddressDetails address = new AddressDetails("Rithesh", "Menda", "ganjimut,mangalore", "karnataka", 9741932439, 574144, "ritheshmenda123@gmail.com");
+                address.Contacts();
+         
         }
     }
 }
